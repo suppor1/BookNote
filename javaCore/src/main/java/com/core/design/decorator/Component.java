0fac5b1d76a56aa0b80299dde60d2a2639b8b4f6
jiangkai <jiangkai05@meituan.com>
2018-09-21 15:46:16 +0800

    装饰者

---
+++ javaCore/src/main/java/com/core/design/decorator/Component.java
@@ -0,0 +1,10 @@
+package com.core.design.decorator;
+
+/**
+ * Created by jiangkai
+ * Date 2018/9/19
+ * 基本的操作，基本对象(被装饰者) 和 装饰类(装饰者) 都可以实现，做自己的事情
+ */
+public interface Component {
+    void operate();
+}
